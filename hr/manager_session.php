ae26ae33f7646d98c5252da0725e4d93c6ab0b03
ain <muharamnurain@gmail.com>
2025-06-03 00:53:28 +0800

    Initial commit

--- hr/manager_session.php
+++ hr/manager_session.php
@@ -1,5 +1,5 @@
 <?php
-include_once $_SERVER['DOCUMENT_ROOT'].'/bcb_berhad/time_session.php';
+include_once __DIR__ . '/../database.php';
 
 $session = new SessionManager(30);
 $session->checkInactivity();
