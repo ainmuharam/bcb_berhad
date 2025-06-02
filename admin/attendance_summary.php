1392cab5a5cb404558d8a26833f407c4b82cc05a
ain <muharamnurain@gmail.com>
2025-06-03 04:07:11 +0800

    Initial commit

--- admin/attendance_summary.php
+++ admin/attendance_summary.php
@@ -1,6 +1,6 @@
 <?php
 date_default_timezone_set('Asia/Kuala_Lumpur');
-include_once $_SERVER['DOCUMENT_ROOT'].'/bcb_berhad/database.php';
+include_once __DIR__ . '/../database.php';
 
 class AttendanceSummary {
     private $conn;
