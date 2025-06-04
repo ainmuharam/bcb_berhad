90b57a72dc460e36e04ef799ea58a299c1423f41
ain <muharamnurain@gmail.com>
2025-06-04 12:42:34 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,9 @@
+import deepface
+import torch
+import torchvision
+import torchaudio
+
+print("DeepFace version:", deepface.__version__)
+print("Torch version:", torch.__version__)
+print("Torchvision version:", torchvision.__version__)
+print("Torchaudio version:", torchaudio.__version__)
