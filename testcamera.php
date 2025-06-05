0b0fa40afe728c4d0cc1254c0760b0411fa4c73a
ain <muharamnurain@gmail.com>
2025-06-05 23:43:27 +0800

    Initial commit

--- testcamera.php
+++ testcamera.php
@@ -1,6 +1,6 @@
 <?php
 // Trigger the python script
-$output = shell_exec('python3 /path/to/camera_script.py 2>&1');
+$output = shell_exec('python3 /path/to/testCamera.py 2>&1');
 
 // Display output from python
 echo "<pre>$output</pre>";
