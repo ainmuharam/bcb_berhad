870d89222fd9692c04e9b4088cbfa1f34bdefa6c
ain <muharamnurain@gmail.com>
2025-06-05 23:54:01 +0800

    Initial commit

--- testcamera.php
+++ testcamera.php
@@ -1,5 +1,7 @@
 <?php
-$output = shell_exec('python3 testCamera.py 2>&1');
+$output = shell_exec('which python3 2>&1');
+echo "Python path: <pre>$output</pre>";
 
-echo "<pre>$output</pre>";
+$output_version = shell_exec('python3 --version 2>&1');
+echo "Python version: <pre>$output_version</pre>";
 ?>
