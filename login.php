8852096951c171aadecffefd270cabb9c6118624
ain <muharamnurain@gmail.com>
2025-06-14 02:23:22 +0800

    Initial commit

--- login.php
+++ login.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 session_start();
-require_once 'database.php';
+include_once __DIR__ . '/database.php';
 
 include_once 'admin/depart_handling.php';
 include_once 'admin/register_handling.php';
